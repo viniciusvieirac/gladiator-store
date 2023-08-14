cebadca55be03e1e59435e819d80c9d6a236bd7c
vieira <vini.clau0@gmail.com>
2023-08-14 20:05:22 -0300

    feat: req01 - rota post products tests

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -3,6 +3,7 @@ import app  from '../../../src/app';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import productsMock from '../../mocks/productsMock';
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
@@ -31,5 +32,12 @@ describe('POST /products', function () {
     expect(httpResponse.body).to.be.deep.equal({
       "message": "orderId is required"
     });
+  })
+  it('correct body should return 201', async function () {
+   
+    const hostInstance = ProductModel.build(productsMock.validBody);
+    sinon.stub(ProductModel, 'create').resolves(hostInstance);
+    const httpResponse = await chai.request(app).post('/products').send(productsMock.validBody);
+    expect(httpResponse.status).to.equal(201);
   })
 });
--- tests/mocks/productsMock.ts
+++ tests/mocks/productsMock.ts
@@ -3,8 +3,8 @@ const validprice = '30 peças de ouro';
 const validOrder = 4;
 const noNameBody = { name: '', price: validprice, order: validOrder };
 const noPriceBody = { name: validName, price: '', order: validOrder };
-const noOrderBody = { name: validName, price: validprice, order: '' };
-const validBody = { name: validName, price: validprice, order: validOrder };
+const noOrderBody = { name: validName, price: validprice, orderId: '' };
+const validBody = { name: validName, price: validprice, orderId: validOrder };
 
 export default {
     noNameBody,
